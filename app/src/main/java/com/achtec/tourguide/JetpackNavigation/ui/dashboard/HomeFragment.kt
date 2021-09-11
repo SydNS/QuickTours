b90b1c937d92d5f5771d67710947f757ebeb78a6
SydNS <sydneynsiimbe@gmail.com>
2021-09-12 01:33:52 +0300

    linking te destination on finding the the tourguides

--- app/src/main/java/com/achtec/tourguide/JetpackNavigation/ui/dashboard/HomeFragment.kt
+++ app/src/main/java/com/achtec/tourguide/JetpackNavigation/ui/dashboard/HomeFragment.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.fragment.findNavController
+import com.achtec.tourguide.R
 import com.achtec.tourguide.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -26,6 +31,10 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        _binding!!.findtourguidesforyou.setOnClickListener {
+            findNavController().navigate(R.id.action_nav_home_to_search_for_tour_guide)
+            Toast.makeText(requireActivity(),"here ",Toast.LENGTH_LONG).show()
+        }
 
         return root
     }
