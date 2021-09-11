eb3e122d39ddd7d24e05955e412dc8ce7ffad2bd
SydNS <sydneynsiimbe@gmail.com>
2021-09-12 02:35:18 +0300

    added the R lue file

--- app/src/main/java/com/achtec/tourguide/JetpackNavigation/ui/dashboard/Search_for_tour_guide.kt
+++ app/src/main/java/com/achtec/tourguide/JetpackNavigation/ui/dashboard/Search_for_tour_guide.kt
@@ -1,10 +1,11 @@
-package com.achtec.tourguide
+package com.achtec.tourguide.JetpackNavigation.ui.dashboard
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.achtec.tourguide.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
