32176ef606509181bfe06fade394c69162679e2d
SydNS <sydneynsiimbe@gmail.com>
2021-09-12 03:00:40 +0300

    this is the adapter to display the list of tour guides around

---
+++ app/src/main/java/com/achtec/tourguide/JetpackNavigation/adapters/TourguidesAdapter.kt
@@ -0,0 +1,26 @@
+package com.achtec.tourguide.JetpackNavigation.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TourguidesAdapter(
+    var tourguides:ArrayList<TourguideModel>
+) : RecyclerView.Adapter<TourguidesAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
