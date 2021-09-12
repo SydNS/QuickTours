bcce69ddb37ac2710ebfcd7c57ecf99805a1cc9b
SydNS <sydneynsiimbe@gmail.com>
2021-09-12 03:53:19 +0300

    tour guide model for the listing

--- app/src/main/java/com/achtec/tourguide/JetpackNavigation/TourguideModel.kt
+++ app/src/main/java/com/achtec/tourguide/JetpackNavigation/TourguideModel.kt
@@ -1,3 +1,10 @@
 package com.achtec.tourguide.JetpackNavigation
 
-data class TourguideModel()
+data class TourguideModel(
+    val image: Int,
+    val name: String,
+    val no_plate: String,
+    val location: String,
+    val phone_number: String,
+    val id_number: String
+)
