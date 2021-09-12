00c1d447f7ce580e8c133648261bc2db3b23d0e8
SydNS <sydneynsiimbe@gmail.com>
2021-09-12 21:46:38 +0300

    chaning the link in the drawer to match the projects

--- app/src/main/java/com/achtec/tourguide/JetpackNavigation/DrawerScreenActivity.kt
+++ app/src/main/java/com/achtec/tourguide/JetpackNavigation/DrawerScreenActivity.kt
@@ -35,7 +35,7 @@ class DrawerScreenActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_profile, R.id.nav_gallery, R.id.nav_slideshow
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
