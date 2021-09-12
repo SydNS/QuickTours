53a20e6c6deaf7a7ec221f8bc3adc992ff82e17a
SydNS <sydneynsiimbe@gmail.com>
2021-09-12 22:27:53 +0300

    change the list of menu ids matching the navigation graph frags for the drawerlayout

--- app/src/main/java/com/achtec/tourguide/JetpackNavigation/DrawerScreenActivity.kt
+++ app/src/main/java/com/achtec/tourguide/JetpackNavigation/DrawerScreenActivity.kt
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.achtec.tourguide.R
 import com.achtec.tourguide.databinding.ActivityDrawerScreenBinding
 
+
 class DrawerScreenActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -35,7 +36,7 @@ class DrawerScreenActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_profile, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
