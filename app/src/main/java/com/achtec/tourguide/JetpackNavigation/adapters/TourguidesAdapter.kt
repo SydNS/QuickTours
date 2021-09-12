742496e8fdcfa582de1745688008c6cef5b52948
SydNS <sydneynsiimbe@gmail.com>
2021-09-12 03:52:11 +0300

    made a recylcerview adapter for the tourguides

--- app/src/main/java/com/achtec/tourguide/JetpackNavigation/adapters/TourguidesAdapter.kt
+++ app/src/main/java/com/achtec/tourguide/JetpackNavigation/adapters/TourguidesAdapter.kt
@@ -1,27 +1,106 @@
 package com.achtec.tourguide.JetpackNavigation.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.achtec.tourguide.JetpackNavigation.TourguideModel
+import com.achtec.tourguide.R
 
 class TourguidesAdapter(
-    var tourguides:ArrayList<TourguideModel>
+    var tourguideList: ArrayList<TourguideModel>, var mlistener: OnItemClickListener
 ) : RecyclerView.Adapter<TourguidesAdapter.ViewHolder>() {
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            R.layout.tourguideitemview,
+            parent, false
+        )
+
+        return ViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentItem = tourguideList[position]
+
+
+        holder.order_tourguide_name.text = currentItem.name
+        holder.order_tourguide_location_now.text = currentItem.location
+        holder.order_tourguide_profilepic.setImageResource(currentItem.image)
+        holder.order_tourguide_phonenumber_topofJrid.text = currentItem.phone_number
+        holder.id_number.text = currentItem.id_number
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return tourguideList.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) ,
+        View.OnClickListener{
+
+        val order_tourguide_name: TextView = itemView.findViewById(R.id.tourguide_name)
+        val order_tourguide_profilepic: ImageView =
+            itemView.findViewById(R.id.order_tourguide_profilepic)
+        val order_tourguide_location_now: TextView =
+            itemView.findViewById(R.id.order_tourguide_location_now)
+        val order_tourguide_phonenumber_topofJrid: TextView =
+            itemView.findViewById(R.id.order_tourguide_phonenumber_topofJrid)
+        val id_number: TextView = itemView.findViewById(R.id.order_tourguide_JRID)
+        val callbutton: ImageView = itemView.findViewById(R.id.order_tourguide_callbutton)
+        val order_tourguide_choose: ImageView = itemView.findViewById(R.id.order_tourguide_choose)
+
+
+        init {
+            itemView.setOnClickListener(this)
+            callbutton.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    mlistener.onCallClick(position)
+
+                }
+            }
+            var x = 1
+            order_tourguide_choose.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    mlistener.onSelectingRider(position)
+
+                }
+                x += 2
+                if (x % 2 == 0) {
+                    order_tourguide_choose.setImageResource(R.drawable.choose)
+
+                } else {
+                    order_tourguide_choose.setImageResource(R.drawable.choose)
+
+                }
+
+            }
+        }
+
+        override fun onClick(v: View?) {
+            val position = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                mlistener.onItemClick(position)
+            }
+
+        }
+
+
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+        fun onCallClick(position: Int)
+        fun onSelectingRider(position: Int)
+
+    }
+
+
+    fun setOnItemClickListener(listener: OnItemClickListener?) {
+        if (listener != null) mlistener = listener
     }
 }
+
