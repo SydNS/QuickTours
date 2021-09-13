ef6ea2dec941f3e22f7c73c62ae18e6b3131e0a6
SydNS <sydneynsiimbe@gmail.com>
2021-09-13 03:38:04 +0300

    this is on the user side

--- app/src/main/java/com/achtec/tourguide/JetpackNavigation/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/achtec/tourguide/JetpackNavigation/ui/profile/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.achtec.tourguide.JetpackNavigation.ui.gallery
+package com.achtec.tourguide.JetpackNavigation.ui.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.achtec.tourguide.databinding.FragmentGalleryBinding
+
 
 class ProfileFragment : Fragment() {
 
