8c04e4dba873508f611a693ffc836dcea55280e5
SydNS <sydneynsiimbe@gmail.com>
2021-09-14 11:02:16 +0300

    suppresed deprecations

--- app/src/main/java/com/achtec/tourguide/SplashFragment.kt
+++ app/src/main/java/com/achtec/tourguide/SplashFragment.kt
@@ -1,10 +1,25 @@
+@file:Suppress("DEPRECATION")
 package com.achtec.tourguide
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.navigation.fragment.findNavController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.NavHostFragment
+import com.achtec.tourguide.databinding.FragmentSplashBinding
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,11 +30,15 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  * Use the [SplashFragment.newInstance] factory method to
  * create an instance of this fragment.
+ * create an instance of requireActivity() fragment.
  */
 class SplashFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var binding: FragmentSplashBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,12 +54,55 @@ class SplashFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
+        // Inflate the layout for requireActivity() fragment
+        binding = FragmentSplashBinding.inflate(layoutInflater)
+
+//        (activity as Activity?).hideActionbar()
+//        (activity as Activity?).setDrawerEnabled(False)
+
+        binding.appTitle.animation =
+            AnimationUtils.loadAnimation(requireActivity(), R.anim.top_anim)
+        binding.enterButton.animation =
+            AnimationUtils.loadAnimation(requireActivity(), R.anim.left_anim)
+        binding.findBestGuide.animation =
+            AnimationUtils.loadAnimation(requireActivity(), R.anim.right_anim)
+        binding.developer.animation =
+            AnimationUtils.loadAnimation(requireActivity(), R.anim.bottom_anim)
+
+        binding.enterButton.setOnClickListener {
+
+            Toast.makeText(activity,"rehtrhtr",Toast.LENGTH_SHORT).show()
+            NavHostFragment.findNavController(this).navigate(R.id.action_splashFragment_to_loginFrag)
+        }
+
+        Handler().postDelayed({
+            checkUserSignupStatus()
+        }, 3000)
+
+
+
+        return binding.root
+
     }
 
+
+    override fun onResume() {
+        super.onResume()
+        val supportActionBar: ActionBar? = (requireActivity() as AppCompatActivity).supportActionBar
+        supportActionBar?.hide()
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (requireActivity() as AppCompatActivity).supportActionBar?.show()
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
+         * Use requireActivity() factory method to create a new instance of
+         * requireActivity() fragment using the provided parameters.
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
@@ -55,5 +117,43 @@ class SplashFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+
+    private fun checkUserSignupStatus() {
+        when (getSignupStatus()) {
+            true -> NavHostFragment.findNavController(this).navigate(R.id.action_splashFragment_to_loginFrag)
+            false -> NavHostFragment.findNavController(this).navigate(R.id.action_splashFragment_to_signupFragment)
+        }
+    }
+
+    private fun getSignupStatus(): Boolean {
+        val sharedPreferences =
+            activity?.getSharedPreferences("Signed_Up", Context.MODE_PRIVATE)
+
+        return sharedPreferences!!.getBoolean("Signed_Up", false)
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                    if ((PackageManager.PERMISSION_GRANTED === ContextCompat.checkSelfPermission(requireActivity(),
+                            Manifest.permission.ACCESS_FINE_LOCATION))) {
+                        Toast.makeText(activity, "Permission Granted", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(activity, "Permission Denied", Toast.LENGTH_SHORT).show()
+//                    onStop()
+                }
+                return
+            }
+        }
+
+
     }
 }
