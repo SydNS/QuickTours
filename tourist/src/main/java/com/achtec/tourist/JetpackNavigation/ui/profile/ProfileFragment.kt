b6e5e6eec473b957950fbfb28b7a1d4428ddd628
SydNS <sydneynsiimbe@gmail.com>
2021-09-17 03:45:44 +0300

    added synthentics

--- tourist/src/main/java/com/achtec/tourist/JetpackNavigation/ui/profile/ProfileFragment.kt
+++ tourist/src/main/java/com/achtec/tourist/JetpackNavigation/ui/profile/ProfileFragment.kt
@@ -5,10 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.achtec.tourist.databinding.FragmentGalleryBinding
+import com.achtec.tourist.databinding.TourguideitemviewBinding
+import kotlinx.android.synthetic.main.fragment_gallery.*
 
 
 class ProfileFragment : Fragment() {
@@ -31,6 +34,9 @@ class ProfileFragment : Fragment() {
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        order_now.setOnClickListener {
+            Toast.makeText(activity,"Nkutuse",Toast.LENGTH_SHORT).show()
+        }
 
         return root
     }
