beb1db41434aa3494a94fe63475ab4abe527fdd2
SydNS <sydneynsiimbe@gmail.com>
2021-09-17 14:17:18 +0300

    modified

---
+++ tourist/src/main/java/com/achtec/tourist/JetpackNavigation/TouristMap.kt
@@ -0,0 +1,12 @@
+package com.achtec.tourist.JetpackNavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.achtec.tourist.R
+
+class TouristMap : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tourist_map)
+    }
+}
