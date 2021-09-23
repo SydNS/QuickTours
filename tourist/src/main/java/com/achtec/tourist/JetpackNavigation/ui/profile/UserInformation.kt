1f3cc6c0455f3a6ed1e92dbd68e4027ba5d9a461
SydNS <sydneynsiimbe@gmail.com>
2021-09-23 06:01:14 +0300

    this will be the user information

---
+++ tourist/src/main/java/com/achtec/tourist/JetpackNavigation/ui/profile/UserInformation.kt
@@ -0,0 +1,9 @@
+package com.achtec.tourist.JetpackNavigation.ui.profile
+
+data class UserInformation(
+    val name: String,
+    val emailAddress: String,
+    val phone_number: String,
+    val usertype: String
+
+)
